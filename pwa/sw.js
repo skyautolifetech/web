cf371f62ef24e97a1dafddde50c5d9df7ee6f4ba
NN-ALT <network@autolifetech.com>
2025-07-01 14:29:24 +0800

    a8-13 pwa v4 patch1

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,7 +1,12 @@
 const cacheName = 'alt-v4';
 const staticAssets = [
   './a8-13.html',
-  './main.css',
+  './style.css',
+  './css/animate.css',
+  './css/flaticon.css',
+  './css/index.css',
+  './css/magnific-popup.css',
+  './main.js',
   './fonts/flaticon.eot',
   './fonts/flaticon.svg',
   './fonts/flaticon.ttf',
@@ -22,11 +27,6 @@ const staticAssets = [
   './media/svg/room_a_v4.svg',
   './media/video/fpold1.mp4',
   './media/video/testintro1.mp4',
-  './scripts/custom.js',
-  './scripts/index.js',
-  './scripts/jquery-3.7.0.min.js',
-  './scripts/jquery.magnific-popup.min.js',
-  './scripts/wow.js',
 ];
 self.addEventListener('install', async e => {
   const cache = await caches.open(cacheName);
