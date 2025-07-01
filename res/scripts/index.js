b2f30042705e755ace0632cc3c976e98a170296e
NN-ALT <network@autolifetech.com>
2025-07-01 12:14:45 +0800

    a8-12 pwa v2

--- res/scripts/index.js
+++ res/scripts/index.js
@@ -25,7 +25,7 @@ function ShowHideView(targetid, selfid) {
 async function registerSW() {
   if ('serviceWorker' in navigator) {
     try {
-      await navigator.serviceWorker.register('./res/sw.js');
+      await navigator.serviceWorker.register('./sw.js');
     } catch (e) {
       console.log(`SW registration failed`);
     }
