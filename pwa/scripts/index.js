f6c34b5b4458e16bc0de8d281d906fd5b39ecd64
NN-ALT <network@autolifetech.com>
2025-07-02 11:57:12 +0800

    .

--- pwa/scripts/index.js
+++ pwa/scripts/index.js
@@ -2,42 +2,16 @@ window.addEventListener('load', () => {
   registerSW();
 });
 
-function showPopup(imgId, overlayId) {
+function showPopup(imgId) {
   const image = document.getElementById(imgId);
-  const overlayCheck = document.getElementById(overlayId);
-  switch (overlayCheck) {
-    case "overlay-dome":
-      const overlay = document.getElementById('overlay-dome' + imgId.replace(/\D/g, ''));
-      break;
-    case "overlay-bullet":
-      const overlay = document.getElementById('overlay-bullet' + imgId.replace(/\D/g, ''));
-      break;
-    case "overlay-car":
-      const overlay = document.getElementById('overlay-car' + imgId.replace(/\D/g, ''));
-      break;
-    default:
-      break;
-  }
+  const overlay = document.getElementById('overlay-' + imgId.replace(/\D/g, ''));
   image.classList.add('active');
   overlay.classList.add('active');
   document.body.style.overflow = 'hidden';
 }
-function hidePopup(imgId, overlayId) {
+function hidePopup(imgId) {
   const image = document.getElementById(imgId);
-  const overlayCheck = document.getElementById(overlayId);
-  switch (overlayCheck) {
-    case "overlay-dome":
-      const overlay = document.getElementById('overlay-dome' + imgId.replace(/\D/g, ''));
-      break;
-    case "overlay-bullet":
-      const overlay = document.getElementById('overlay-bullet' + imgId.replace(/\D/g, ''));
-      break;
-    case "overlay-car":
-      const overlay = document.getElementById('overlay-car' + imgId.replace(/\D/g, ''));
-      break;
-    default:
-      break;
-  }
+  const overlay = document.getElementById('overlay-' + imgId.replace(/\D/g, ''));
   image.classList.remove('active');
   overlay.classList.remove('active');
   document.body.style.removeProperty('overflow');
